5de703f84cef48aa47f008d046aec2f1b0570c1d
徐晨 <kimixuchen@163.com>
2015-09-23 23:22:01 +0800

    queue is int only

--- myPriorityQueue.java
+++ myPriorityQueue.java
@@ -1,38 +1,47 @@
-package t1;
+package datastructures;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class myPriorityQueue<T>{
+public class myPriorityQueue{
 
-	private ArrayList<T> list;
+	private int list[];
 	int size;
+	int pos;
 	
-	public myPriorityQueue(){
-		list = new ArrayList<T>();
-		size=0;
+	public myPriorityQueue(int n){
+		list = new int[n];
+		size=n;
+		pos=0;
 	}
 	
-	public T extractMax(){
-		T max = list.get(0);
-		list.remove(0);
+	public int extractMax(){
+		if(size == 0){
+			System.out.println("queue is empty");
+			return Integer.MIN_VALUE;
+		}
+		int max = list[1];
+		for(int i=2; i<size; i++)
+			list[i-1] = list[i];
 		size--;
 		return max;
 	}
 	
-	public T max(){
-		return list.get(0);
+	public int max(){
+		if(size == 0){
+			System.out.println("queue is empty");
+			return Integer.MIN_VALUE;
+		}
+		return list[1];
 	}
 	
-	public void insert(T x){
-		list.add(x);
-		heapUp(list.size()-1);
-		size++;
+	public void insert(int x){
+		list[++pos] = x;
+		heapUp(pos);
 	}
 	
-	public void increaseKey(int i, T x){
-		Comparable<T> key = (Comparable) x;
-		if(key.compareTo(list.get(i))<0){
+	public void increaseKey(int i, int x){
+		if(x<list[i]){
 			System.out.println("new key is smaller than current key");
 			return;
 		}
@@ -40,18 +49,44 @@ public class myPriorityQueue<T>{
 	}
 	
 	public void heapDown(int i){
-		if(2*i<size){
-			
+		int j, k, x;
+		k = i;
+		j=2*k;
+		x = list[k];
+		while(j<size){
+			if(list[j]<list[j+1]) j++;
+			if(x<list[j]){
+				list[k] = list[j];
+				k = j;
+				j<<=1;
+			}
+			else
+				break;
 		}
+		list[k] = x;
 	}
 	
 	public void heapUp(int i){
-		
+		int j, k, x;
+		k = i;
+		x = list[k];
+		while(k>1 && x>list[k/2]){
+			list[k] = list[k/2];
+			k>>=1;
+		}
+		list[k] = x;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		myPriorityQueue queue = new myPriorityQueue(10);
+		queue.insert(4);
+		queue.insert(7);
+		queue.insert(90);
+		queue.insert(45);
+		queue.insert(1);
+		for(int i=0; i<5; i++)
+			System.out.println(queue.extractMax());
 	}
 
 }
